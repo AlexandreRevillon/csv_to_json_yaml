e0588d87f8d9cb6bbfba4dc2fd006f6133767391
Alexandre <alexandre.revillon@outlook.fr>
2023-03-01 17:26:32 +0100

    Gestion des arguments du main

--- src/main.c
+++ src/main.c
@@ -1,14 +1,16 @@
 /**
- * @brief      This file initializes arrays and sort them using 3 different algorithms
+ * @brief      ...
  *
- * @author     Jeremie
- * @date       2020
+ * @author     Alexandre REVILLON & Arthur DUPIRE
+ * @date       2023
  */
+
 #include <memory.h>
-#include <sort_algorithms.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <utils.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 /**
  * @brief      Compares 3 arrays of integers
@@ -20,18 +22,54 @@
  *
  * @return     { description_of_the_return_value }
  */
-bool compare_tab (int t1[], int t2[], int t3[], unsigned int N)
+// bool compare_tab (int t1[], int t2[], int t3[], unsigned int N)
+// {
+//     
+// }
+
+
+int main (int argc, char* argv[])
 {
-    for (unsigned int i = 0; i < N; i++)
-    {
-        if ((t1[i] != t2[i]) || (t2[i] != t3[i])) return false;
-    }
-    return true;
-}
+    int i = 1;
+    char exit_format[4] = "yaml";
+    char name_file[50];
+    bool save = 0;
+    char col_nahs[4];
+    char affi[50];
 
-
-
-int main ()
-{
+    //Parcours des argument du main
+        while(i < argc-1)  /* Skip argv[0] (program name). */
+        {
+            if (strcmp(argv[i], "-t") == 0)  /* Process optional arguments. */
+            {
+                strcpy(exit_format, argv[i+1]);
+            }
+            else if  (strcmp(argv[i], "-o") == 0)
+            {
+                save = 1;
+                strcpy(col_nahs, argv[i+1]);
+            }
+            else if  (strcmp(argv[i], "-f") == 0)
+            {
+                strcpy(col_nahs, argv[i+1]);
+            }
+            else if  (strcmp(argv[i], "-s") == 0)
+            {
+                strcpy(affi, argv[i+1]);
+            }
+            else
+            {
+                fprintf( stderr, "%s : Argument non reconnu\n", argv[i]);
+                exit(1);
+            }
+            i += 2;
+        }
+    
+    
+    
+    
+    
+    
+    
     return 0;
 }
